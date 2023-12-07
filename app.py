80106c2a9224062666ddafcd1a6f53bfa0ebc8b3
Javier Cervantes <33461065+cervs257@users.noreply.github.com>
2023-12-07 18:25:54 +0000

    fixed plot and htmls

--- app.py
+++ app.py
@@ -3,26 +3,43 @@ import io
 import base64
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import yfinance as yf
+import numpy as np
 
 
 # download 30 min data for the last 7 days for the given ticker
 def get_data(ticker):
-    data = yf.download(ticker, period="7d", interval="30m")
+    data = yf.download(ticker, period="7d", interval="5m")
     # add title to the dataframe
     data["Ticker"] = ticker
     return data
 
 
 # create a plot of the data and save it to a temporary buffer
+
+
 def create_plot(data):
-    ticker = data["Ticker"][0]
+    ticker_name = data["Ticker"][0]
+
     fig, ax = plt.subplots()
-    ax.plot(data["Close"])
-    ax.set_title("Close Price of " + ticker)
+
+    # Convert the index to integers
+    ax.plot(range(len(data)), data["Close"])
+
+    ax.set_title("Close Price of " + ticker_name)
     ax.set_xlabel("Date")
     ax.set_ylabel("Price")
     ax.grid()
+
+    # Create a function to format the x-axis ticks
+    def format_date(x, pos=None):
+        thisind = np.clip(int(x + 0.5), 0, len(data.index) - 1)
+        return data.index[thisind].strftime("%Y-%m-%d")
+
+    # Set the x-axis formatter
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+
     fig.autofmt_xdate()
     buf = io.BytesIO()
     plt.savefig(buf, format="png")
@@ -37,7 +54,8 @@ app = Flask(__name__)
 # define the root route
 @app.route("/")
 def index():
-    return render_template("index.html")
+    ticker = "MSFT"
+    return render_template("index.html", ticker=ticker)
 
 
 # define the route for the plot image
@@ -47,9 +65,8 @@ def plot_png():
     ticker = "MSFT"
     data = get_data(ticker)
     buf = create_plot(data)
-    # embed the plot in the html output
-    data = base64.b64encode(buf.read()).decode("ascii")
-    return render_template("index.html", ticker=ticker, data=data)
+    buf.seek(0)
+    return send_file(buf, mimetype='image/png')
 
 if __name__ == "__main__":
     app.run(debug=True)
